aa93851141884a47b69cb1ad56360fc9cc6905e0
Vraj911 <vraj9112005@gmail.com>
2026-05-08 20:20:57 +0530

    fixed next config

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,24 +1,21 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    externalDir: true,
+  },
+  outputFileTracingRoot: path.join(__dirname, "../"),
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [
         ...(config.externals || []),
-        'monaco-editor',
+        "monaco-editor",
       ];
     }
+
     return config;
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
-      },
-    ];
-  },
-}
-
-module.exports = nextConfig
+};
+module.exports = nextConfig;
